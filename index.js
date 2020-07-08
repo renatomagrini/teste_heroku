620dcdc7bf57fe60434ad1c5529c78b88564cafe
renato <renatodmagrin@gmail.com>
2020-07-08 11:32:21 -0300

    test1

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ let express = require('express'),
     path = require('path');
 var app = express();
 let server = require('http').Server(app);
-const port = process.env.PORT || 8000;
+const PORT = process.env.PORT||'8080'
 
 
 
-server.listen(port,() => {
-	console.log("App is running on port" + port);
+server.listen(PORT,() => {
+	console.log("App is running on port" + PORT);
 });
 
 app.use(express.static(path.join(__dirname)));
